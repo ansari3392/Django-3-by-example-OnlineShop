8a68a19ae96d8229d4ee48c91d20b5d28a6b7a81
fatemeh <ansari.3392@gmail.com>
2021-08-26 09:53:01 +0430

    Feat(cart--views): updating product quantity in the cart

--- cart/views.py
+++ cart/views.py
@@ -2,14 +2,14 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.views.decorators.http import require_POST
 from shop.models import Product
 from .cart import Cart
-from .forms import CartProductForm
+from .forms import CartAddProductForm
 
 
 @require_POST
 def cart_add(request, product_id):
     cart = Cart(request)
     product = get_object_or_404(Product, id=product_id)
-    form = CartProductForm(request.POST)
+    form = CartAddProductForm(request.POST)
     if form.is_valid():
         cd = form.cleaned_data
         cart.add(product=product, quantity=cd['quantity'], override_quantity=cd['override'])
@@ -20,15 +20,20 @@ def cart_add(request, product_id):
 @require_POST
 def cart_remove(request, product_id):
     cart = Cart(request)
-    product = get_object_or_404(Product, product_id)
+    product = get_object_or_404(Product, id=product_id)
     cart.remove(product)
     return redirect('cart:cart_detail')
 
 
 
 def cart_detail(request):
-    cart = Cart(request)
-    return render(request, 'cart:detail.html', {'cart': cart})
+    cart = Cart(request) 
+    for item in cart:
+        item['update_quantity_form']= CartAddProductForm(initial={
+            'quantity':item['quantity'],
+            'override' : True
+        })
+    return render(request, 'cart/detail.html', {'cart': cart})
 
 
 
