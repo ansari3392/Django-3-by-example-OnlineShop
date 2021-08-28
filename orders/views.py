4057962af870bbd48031e3cacf9169c30a8b5c62
fatemeh <ansari.3392@gmail.com>
2021-08-28 16:16:48 +0430

    Feat(orders--view): add adminorderdetailview

--- orders/views.py
+++ orders/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.urls import reverse
 from django.views.generic import ListView, CreateView, TemplateView, UpdateView, DeleteView, DetailView
+from django.contrib.admin.views.decorators import staff_member_required
 from cart.cart import Cart
-from .models import OrderItem
+from .models import OrderItem, Order
 from .forms import OrderCreateForm
 from .tasks import order_created
 
@@ -31,3 +32,12 @@ class OrderCreateView(CreateView):
         cart = Cart(self.request)
         form = OrderCreateForm()
         return render(request, 'orders/order/create.html', {'cart':cart, 'form':form})
+
+@staff_member_required
+class AdminOrderDetail(DetailView):
+    def get_context_data(self, **kwargs):
+        context = super(AdminOrderDetail, self).get_context_data(**kwargs)
+        context['order'] = get_object_or_404(Order, id=order_id)
+        return context
+
+    template_name = 'admin/orders/order/detail.html'
