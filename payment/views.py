a98e5b56678025dcde89a8d9c6a84cae4e95bb07
fatemeh <ansari.3392@gmail.com>
2021-08-28 11:56:34 +0430

    Feat(paymen--view): added paymentprocess view

--- payment/views.py
+++ payment/views.py
@@ -1,3 +1,43 @@
-from django.shortcuts import render
+import braintree
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views.generic import ListView, CreateView, TemplateView, UpdateView, DeleteView, DetailView, View
+from django.conf import settings
+from orders.models import Order
 
-# Create your views here.
+#instantiate braintree payment gateway
+gateway = braintree.BraintreeGateway(settings.BRAINTREE_CONF)
+
+class PaymentProcessView(View):
+    def post(self, request):
+        order_id = self.request.session.get('order_id')
+        order = get_object_or_404(Order, id=order_id)
+        get_total_cost = order.get_total_cost()
+        #retrieve nonce
+        nonce = self.request.POST.get('payment_method_nonce', None)
+        #create and submit transaction
+        result = gateway.transaction.sale({
+            'amount': f'{total_cost:2f}',
+            'payment_method_nonce': nonce, 
+            'options': {
+                'submit_for_settlement': True
+            }
+        })
+        if result.is_success:
+            #mark the order as paid
+            order.paid = True
+            #store the unique transaction id 
+            order.braintree_id = result.transaction.id
+            order.save()
+            return redirect('payment:done')
+        else:
+            return redirect('payment:canceled')
+
+    def get(self, request):
+        order_id = self.request.session.get('order_id')
+        order = get_object_or_404(Order, id=order_id)
+        get_total_cost = order.get_total_cost()
+        #generate token
+        client_token = gateway.client_token.generate()
+        return render(request, 'payment/process.html', {'order':order, 'client_token':client_token})
+
+
