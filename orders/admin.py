40d8701ee19e199bb4e6b32e662b0f7c73498beb
fatemeh <ansari.3392@gmail.com>
2021-08-28 17:25:26 +0430

    Feat(orders--admin): add order detail fn

--- orders/admin.py
+++ orders/admin.py
@@ -2,6 +2,8 @@ import csv
 import datetime
 from django.http import HttpResponse
 from django.contrib import admin
+from django.urls import reverse
+from django.utils.safestring import mark_safe
 from .models import Order, OrderItem
 
 def export_to_csv(modeladmin, request, queryset):
@@ -33,10 +35,18 @@ class OrderItemInline(admin.TabularInline):
     raw_id_fields = ['product']
 
 
+
+def order_detail(obj):
+    url = reverse('orders:admin_order_detail', args=[obj.id])
+    return mark_safe(f'<a href="{url}">View</a>')
+
+
+
+
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
     list_display = ['id', 'first_name', 'last_name', 'email', 
-                    'address', 'postal_code', 'city', 'paid', 'created', 'updated']
+                    'address', 'postal_code', 'city', 'paid', 'created', 'updated', order_detail]
     list_filter = ['paid', 'created', 'updated']
     inlines = [OrderItemInline]
     actions =[export_to_csv]
