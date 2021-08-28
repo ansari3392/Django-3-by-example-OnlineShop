01d2481bb948e3967fe5e4b3b1ee4d4acb9b2789
fatemeh <ansari.3392@gmail.com>
2021-08-28 15:02:25 +0430

    Feat(orders--admin): create export to csv fn

--- orders/admin.py
+++ orders/admin.py
@@ -1,6 +1,31 @@
+import csv
+import datetime
+from django.http import HttpResponse
 from django.contrib import admin
-
 from .models import Order, OrderItem
+
+def export_to_csv(modeladmin, request, queryset):
+    opts = modeladmin.model._meta 
+    content_disposition = 'attachment; filename={opts.verbose_name}.csv'
+    response = HttpResponse(content_type='text/csv')
+    response['content-Disposition'] = content_disposition
+    writer = csv.writer(response)
+
+    fields =[ field for field in opts.get_fields() if not field.many_to_many and not field.one_to_many]
+    writer.writerow([field.verbose_name for field in fields])
+
+    for obj in queryset:
+        data_row =[]
+        for field in fields:
+            value = getattr(obj, field.name)
+            if isinstance(value, datetime.datetime):
+                value = value.strftime('%d/%m/%y')
+            data_row.append(value)
+        writer.writerow(data_row)
+    return response
+
+    export_to_csv.short_description = 'Export to csv'
+
 
 
 class OrderItemInline(admin.TabularInline):
