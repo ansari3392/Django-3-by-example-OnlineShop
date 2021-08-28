fd9a9c53d7253df3960d1bc9e15f64f71ed049c1
fatemeh <ansari.3392@gmail.com>
2021-08-28 16:29:36 +0430

    Fix(orders--view): remove decorator`

--- orders/views.py
+++ orders/views.py
@@ -33,8 +33,8 @@ class OrderCreateView(CreateView):
         form = OrderCreateForm()
         return render(request, 'orders/order/create.html', {'cart':cart, 'form':form})
 
-@staff_member_required
-class AdminOrderDetail(DetailView):
+
+class AdminOrderDetailView(DetailView):
     def get_context_data(self, **kwargs):
         context = super(AdminOrderDetail, self).get_context_data(**kwargs)
         context['order'] = get_object_or_404(Order, id=order_id)
