c909bddcbb4d4d559a10100c81e050e60df6eb21
fatemeh <ansari.3392@gmail.com>
2021-08-28 16:22:23 +0430

    Feat(orders--url): add a new path

--- orders/urls.py
+++ orders/urls.py
@@ -6,5 +6,6 @@ app_name = 'orders'
 
 urlpatterns = [
     path('create/', views.OrderCreateView.as_view(), name='order_create'),
+    path('admin/order/<int:order_id>/', views.AdminOrderDetail.as_view(), name='admin_order_detail'),
     
 ]
