3fe9e634c5fd0a5a05c41f3a97e63175db55b439
fatemeh <ansari.3392@gmail.com>
2021-08-30 10:55:03 +0430

    Feat(myshomp--setting): set internationalization config

--- myshop/settings.py
+++ myshop/settings.py
@@ -56,6 +56,7 @@ INSTALLED_APPS.append('coupons.apps.CouponsConfig')
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -128,6 +129,14 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
+
+LANGUAGES = (
+    ('en', 'English'),
+    ('es', 'Spanish'),
+    ('fa', 'Persian'),
+    ('ar', 'Arabic'),
+    
+)
 
 
 # Static files (CSS, JavaScript, Images)
