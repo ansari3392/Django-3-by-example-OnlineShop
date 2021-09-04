89fc718b8679c9f7a77b267bf12afcce04418957
fatemeh <ansari.3392@gmail.com>
2021-09-04 13:30:39 +0430

    Faet(coupons--app): coupon app is now finished

--- coupons/views.py
+++ coupons/views.py
@@ -3,23 +3,23 @@ from django.utils import timezone
 from django.views.decorators.http import require_POST
 from django.views.generic import CreateView, View
 from django.utils.decorators import method_decorator
+from django.core.exceptions import ObjectDoesNotExist
 from .models import Coupon
 from .forms import CouponApplyForm
 
 @method_decorator(require_POST, name='dispatch')
-class CouponApply(CreateView):
+class CouponApply(View):
     def post(self, request,*args, **kwargs):
-        now = timezone.now
+        now = timezone.now()
         form = CouponApplyForm(request.POST,)
         
         if form.is_valid():
-            order = form.save()
             code = form.cleaned_data['code']
             try:
-                coupon = Coupon.objects.get(code__exact=code, valid_from__lte=now, valid_to__gte=now, active=True)
+                coupon = Coupon.objects.get(code__iexact=code, valid_from__lte=now, valid_to__gte=now, active=True)
                 request.session['coupon_id'] = coupon.id
-            except Coupon.DoeseNotExist:
-                request.session['coupon_id'] = None
+            except ObjectDoesNotExist:
+                request.session['coupon_id'] = 123 
                 
         return redirect('cart:cart_detail')
 
